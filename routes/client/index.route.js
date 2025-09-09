559477386b8197c194652fa5d2bd15b41021f118
JungNguyen <minhdung38109141@gmail.com>
2025-09-09 23:43:07 +0700

    Add main route file

---
+++ routes/client/index.route.js
@@ -0,0 +1,11 @@
+const productRoutes = require("./products.route.js");
+const homeRoutes = require("./home.route.js");
+
+module.exports = (app) => {         // export route và tham số app ỏ file main
+
+// Route trang chủ (home)
+  app.use("/", homeRoutes);
+
+// Route trang products
+  app.use("/products", productRoutes);        // since router to products is available, we just USE
+} 
