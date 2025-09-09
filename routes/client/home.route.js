9c58b115c26fc57e9bbc075520a35ba74498ced2
Nguyễn Minh Dũng (Jung Nguyen) <minhdung38109141@gmail.com>
2025-09-09 23:49:38 +0700

    Delete routes/client/home.route.js
    File already existed in the branch feature/home-route

--- routes/client/home.route.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-router.get("/", (req, res) => {
-  res.render("client/pages/home/index.pug");
-});
-
-module.exports = router;   // make this router available outside
