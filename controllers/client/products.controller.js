87a72b7c498654b5c40285d67ab8ec2be92b8a86
JungNguyen <minhdung38109141@gmail.com>
2025-09-10 16:11:44 +0700

    Làm trang ds sản phẩm bên Client

--- controllers/client/products.controller.js
+++ controllers/client/products.controller.js
@@ -1,5 +1,23 @@
-module.exports.index = (req, res) => {        // index là trang chính của controller products
+const Product = require("../../models/product.model.js");
+
+module.exports.index = async (req, res) => {        // index là trang chính của controller products
+// hàm find, truyền vào object -> object là các bản ghi có key status: active và deleted: false 
+  const products = await Product.find({            
+    status: "active",
+    deleted: false
+  });        
+
+
+// tính toán giá mới sau giảm giá
+  const newProducts = products.map((item) => {
+    item.newPrice = (item.price * (100 - item.discountPercentage) / 100).toFixed(2);    // để làm tròn 2 số sau dấu phẩy
+    return item;
+  });
+
+  console.log(newProducts);
+
   res.render("client/pages/products/index.pug", {
-    pageTitle: "Trang danh sách sản phẩm"
+    pageTitle: "Trang danh sách sản phẩm",
+    products: newProducts
   });
 }
