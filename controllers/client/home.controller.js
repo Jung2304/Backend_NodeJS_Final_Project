dd9901adc8689140ac6de4d1f6d77fdac44746b4
JungNguyen <minhdung38109141@gmail.com>
2025-09-10 08:57:10 +0700

    Cấu trúc lại folder PUG, dùng truyền data động cho các controller

--- controllers/client/home.controller.js
+++ controllers/client/home.controller.js
@@ -1,3 +1,5 @@
 module.exports.index = (req, res) => {           // index là trang chính của controller home
-  res.render("client/pages/home/index.pug")
+  res.render("client/pages/home/index.pug", {
+    pageTitle: "Trang chủ"                      // data động
+  })
 }
--- controllers/client/products.controller.js
+++ controllers/client/products.controller.js
@@ -1,3 +1,5 @@
 module.exports.index = (req, res) => {        // index là trang chính của controller products
-  res.render("client/pages/products/index.pug");
+  res.render("client/pages/products/index.pug", {
+    pageTitle: "Trang danh sách sản phẩm"
+  });
 }
