0e0a5c8f27d891db9016325f7fea19d5b941e200
JungNguyen <minhdung38109141@gmail.com>
2025-09-11 11:24:23 +0700

    layout chung
    + cập nhật một số file + sử dụng biến toàn cục

--- config/system.js
+++ config/system.js
@@ -1,7 +1,7 @@
 // File này sẽ chứa những biến hệ thống
 
 // Định nghĩa PATH_ADMIN 
-  const PATH_ADMIN = "/admin";
+const PATH_ADMIN = "/admin";
 
 module.exports = {
   prefixAdmin: PATH_ADMIN
--- index.js
+++ index.js
@@ -1,24 +1,32 @@
 const express = require("express");
+const app = express(); 
 require("dotenv").config();     // cài package dotenv và require như này để dùng các hằng trong file .env
+const systemConfig = require("./config/system.js");     // cài các biến hệ thống thành biến toàn cục
 
+//! lOCAL VARIABLES
+app.locals.prefixAdmin = systemConfig.prefixAdmin;
+
+
+//! DATABASE
 const database = require("./config/database.js");     // ./ start from current dir
 database.connect();       // kết nối database
 
+//! PORT
 const port = process.env.PORT;      // lấy cổng port bên file .env
-const app = express(); 
-
-const routeAdmin = require("./routes/admin/index.route.js");        // dashboard of Admin  
-const routeClient = require("./routes/client/index.route.js");     // main route of Client
-
-app.use(express.static("public"));      // /public -> / -> các file tĩnh đều available từ root /
-
-app.set("views", "./views");
-app.set("view engine", "pug");
-
-// Routes (gọi hàm và truyền đối số)
-routeAdmin(app);
-routeClient(app);      
-
 app.listen(port, () => {
   console.log(`App listening on port ${port}`);
 });
+
+//! MAIN ROUTE
+const routeAdmin = require("./routes/admin/index.route.js");        // dashboard of Admin  
+const routeClient = require("./routes/client/index.route.js");     // main route of Client
+routeAdmin(app);
+routeClient(app);      
+
+//! STATIC FILES va VIEW ENGINE
+app.use(express.static("public"));      // /public -> / -> các file tĩnh đều available từ root /
+app.set("views", "./views");
+app.set("view engine", "pug");
+
+
+
