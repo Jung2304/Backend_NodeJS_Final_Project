810fd876bddde7c011847929238d5e2289795108
JungNguyen <minhdung38109141@gmail.com>
2025-09-13 20:32:46 +0700

    Tính năng tìm kiếm (BE sử dụng regex)

--- controllers/admin/products.controller.js
+++ controllers/admin/products.controller.js
@@ -26,23 +26,31 @@ module.exports.index = async (req, res) => {
   };
 
   //! Nếu có yêu cầu truy vấn status thì ta mới truyền vào + nút bấm chuyển
-  if(req.query.status) {
-    console.log(req);
-
+  if (req.query.status) {
     filterStatus.forEach(item => {
       item.class = (item.status == req.query.status) ? "active" : "";   
     })
-    find.status = req.query.status;
+    find.status = req.query.status;         // gán cho object find một key status
   } 
 
   //! Nếu có yêu cầu tìm kiếm sản phẩm
-  
+  let keyword = "";
 
-  const products = await Product.find(find);        //< sau tất cả logic thì mới tìm kiếm 
+  if (req.query.keyword) {
+    keyword = req.query.keyword;
 
+    const regex = new RegExp(keyword, "i");       // regex để tìm kiếm keyword + không phân biệt
+    find.title = regex;                 // gán cho object find một key title
+  }
+
+  //! Phần dùng model + các logic để trích xuất dữ liệu
+  const products = await Product.find(find);         
+
+  //! Phần truyền ra view
   res.render("admin/pages/products/index", {
     pageTitle: "Danh sách sản phẩm",
     products: products,
-    filterStatus: filterStatus
+    filterStatus: filterStatus,
+    keyword: keyword              // giữ từ tìm kiế m ở ô input
   });
 }
