3bf608f07948ccc3ab3197a72c81a39697e82eb8
JungNguyen <minhdung38109141@gmail.com>
2025-10-04 21:46:46 +0700

    Update index.js (bỏ env lên đầu)

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const express = require("express");
 require("dotenv").config();     // cài package dotenv và require như này để dùng các hằng trong file .env
+const express = require("express");
 const systemConfig = require("./config/system.js");     // cài các biến hệ thống thành biến toàn cục
 const methodOverride = require("method-override");
 const bodyParser = require("body-parser");
