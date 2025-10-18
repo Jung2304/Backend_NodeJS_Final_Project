6b560a3a5eb6663d82287a2c43a2845302293a04
JungNguyen <minhdung38109141@gmail.com>
2025-10-18 10:13:11 +0700

    BE trả quyền và title cho FE xử lý
    FE sẽ xử lý route, ds hiện bên sider, quyền hạn

--- middlewares/admin/auth.middleware.js
+++ middlewares/admin/auth.middleware.js
@@ -1,5 +1,6 @@
 const systemConfig = require("../../config/system.js");
 const Account = require("../../models/account.model.js");
+const Role = require("../../models/role.model.js");
 
 module.exports.requireAuth = async (req, res, next) => {
   const token = req.cookies.token;
@@ -7,11 +8,18 @@ module.exports.requireAuth = async (req, res, next) => {
   if (!token) {
     res.redirect(`${systemConfig.prefixAdmin}/auth/login`);       // nếu kh có cookie thì cho về trang đăng nhập
   } else {
-    const user = await Account.findOne({ token: token });
+    const user = await Account.findOne({ token: token }).select("-password");
 
     if (!user) {
-      res.redirect(`${systemConfig.prefixAdmin}/auth/login`);       // nếu cookie kh tồn tại (kh có user)
+      res.redirect(`${systemConfig.prefixAdmin}/auth/login`);       // nếu cookie kh tồn tại trong DB (kh có user)
     } else {
+      const role = await Role.findOne({ 
+        role_id: user.role_id,
+        deleted: false      
+      }).select("title permissions");       // chỉ cần title và permissions
+
+      res.locals.user = user;       // trả user
+      res.locals.role = role;       // trả role
       next();
     }
   }
